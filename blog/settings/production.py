22e68b34ea07c6e3bc091f5250896962055a6e3c
Dashgin <76277687+dashgin@users.noreply.github.com>
2021-04-30 15:23:40 +0400

    Update production.py

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = False
 # DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'dashgin.herokuapp.com', 'dashgin.me']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'dashgin.herokuapp.com', 'dashgin.me', 'www.dashgin.me']
 
 DATABASES = {
     'default': {
