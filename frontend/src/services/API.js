eae1b04cf4d5cb631bb436bae1691c26f5e5a957
Dashgin <dsdsds2103@gmail.com>
2021-12-02 23:12:32 +0400

    added heroku backend api url

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://dashgin.herokuapp.com/api/v1",
   responseType: "json"
 });
