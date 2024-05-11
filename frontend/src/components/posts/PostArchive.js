cdb6724460433986bbc9212a6317ab786de26be1
Dashgin <dsdsds2103@gmail.com>
2024-05-11 20:24:29 +0400

    nc

--- frontend/src/components/posts/PostArchive.js
+++ frontend/src/components/posts/PostArchive.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import API from '../../services/API'
+import React, { useEffect, useState } from 'react';
 import Skeleton from "react-loading-skeleton";
+import { Link } from 'react-router-dom';
+import API from '../../services/API';
 
 
 const PostArchive = () => {
